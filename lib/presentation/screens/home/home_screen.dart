650e26a5ad0d8af76e8c428e7d640cacb3806e67
FelipeArredond <pipechiqui77@gmail.com>
2024-08-18 16:56:32 -0600

    Added listview to iterate menu items list and list tile to render with format

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
-  
   const HomeScreen({super.key});
 
   @override
@@ -10,6 +10,24 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Widgets in Flutter + Material 3'),
         centerTitle: false,
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(appMenuItems[index].icon),
+        title: Text(appMenuItems[index].tittle),
       ),
     );
   }
