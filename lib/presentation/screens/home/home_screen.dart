4b3c7da8313dc0f465a99b8d398f18009b00509f
FelipeArredond <pipechiqui77@gamil.com>
2024-09-03 19:46:18 -0500

    Basic router implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -34,6 +35,13 @@ class _HomeView extends StatelessWidget {
           leading: Icon(menuItem.icon),
           title: Text(menuItem.tittle),
           subtitle: Text(menuItem.subTittle),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const ButtonsScreen() 
+              )
+            );
+          },
         );
       },
     );
